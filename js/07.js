f0b54981e23d474451106bbe72bfae250933aa5e
m1xture <109725502+m1xture@users.noreply.github.com>
2023-03-09 20:57:59 +0200

    ghj

--- js/07.js
+++ js/07.js
@@ -3,3 +3,30 @@ const hisAge = 12;
 alert(myAge + hisAge);
 const group = "FE-1y";
 console.log("Я навчаюся в GoITeens в групі:", group);
+let number = 10;
+// number = 15 * number;
+number += 5;
+console.log(number);
+let elementWidth = "50px";
+console.log(elementWidth);
+Number.parseInt(elementWidth);
+console.log(Number.parseInt(elementWidth));
+elementWidth = Number.parseInt(elementWidth);
+console.log(elementWidth);
+
+// Number("78");
+// console.log(Number("78"));
+
+// let elementHeight = "200.55px";
+// console.log(elementHeight);
+// elementHeight = Number.parseFloat.(elementHeight);
+// console.log("Hundreds", elementHeight);
+let salary = 1000.46548;
+salary = Number(salary);
+salary = salary.toFixed(0);
+console.log(salary);
+
+let quantity = "30";
+let message = "This line cannot be number";
+console.log(Number(quantity));
+console.log(Number(message));
