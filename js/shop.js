610cf3d6e6f1602e090fbda2a0873ee3142595e3
m1xture <109725502+m1xture@users.noreply.github.com>
2023-03-11 14:13:41 +0200

    dop

---
+++ js/shop.js
@@ -0,0 +1,6 @@
+const name = "MacBook Pro 13″";
+let price = 10000;
+console.log("Обрано", name, "Ціна за штуку", price, "кредитів у банку");
+price = 20000;
+console.log("Обрано", name, "Ціна за штуку", price, "кредитів у банку");
+
