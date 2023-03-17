d83d9c39bceec143b250e0635137e0b7658b31d2
m1xture <109725502+m1xture@users.noreply.github.com>
2023-03-17 19:48:40 +0200

    msth

---
+++ js/hw3/math.js
@@ -0,0 +1,18 @@
+const k1 = Number.parseInt("5px");
+const k2 = Number.parseInt("12djd334");
+const k3 = Number.parseInt("12.45asdwe12");
+const k4 = Number.parseInt("qwqweeewq");
+console.log(k1, k2, k3, k4);
+
+const c1 = Number.parseFloat("5px");
+const c2 = Number.parseFloat("12djd334");
+const c3 = Number.parseFloat("12.45asdwe12");
+const c4 = Number.parseFloat("qwqweeewq");
+console.log(c1, c2, c3, c4);
+
+const m1 = Math.min(2, 34, 99, 3, 22, 36, 733, 18);
+const m2 = Math.max(2, 34, 99, 3, 22, 36, 733, 18);
+console.log("найбільше - " + m2 + " " + "найменше -" + " " + m1);
+
+const randomNumber = Math.round(Math.random() * (19 - 3) + 3);
+console.log(randomNumber);
