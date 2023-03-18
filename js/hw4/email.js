b16628aa4efb920dc6964901b858af06bb696c4d
m1xture <109725502+m1xture@users.noreply.github.com>
2023-03-18 14:35:19 +0200

    hw4

---
+++ js/hw4/email.js
@@ -0,0 +1,13 @@
+const email = "misha4090@gmail.com";
+console.log(email.includes("@"));
+
+
+const my = "My";
+const named = "name";
+const is = "is";
+const fullName = my + " " + named + " " + is + " " + "Michael";
+console.log(fullName);
+
+const userName = "Viktor";
+const payment = 250;
+console.log("Thanks," + " " + userName + "." + " You have to pay" + " " + payment);
