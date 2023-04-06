d4ad827955761f8b0cd05ebe505a9ee276497273
m1xture <109725502+m1xture@users.noreply.github.com>
2023-04-06 17:56:49 +0300

    i hope you`ll get it

---
+++ js/hw7/hw7.js
@@ -0,0 +1,19 @@
+//todo: #1
+
+// const a1 = 5;   // ? Global
+// if (true) {
+//     console.log(a1);
+//     const a2 = 88; //? Local
+// }
+// console.log(a2);
+
+const b1 = 45; //? Global
+if (true) {
+  const b2 = 33; //? Local
+  if (true) {
+    console.log(b2); //?
+  }
+}
+if (true) {
+  console.log(b2);
+}
