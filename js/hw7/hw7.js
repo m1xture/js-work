7edf4a2cd2ae6f5cc300686b1f955e91f60fd22b
m1xture <109725502+m1xture@users.noreply.github.com>
2023-04-08 15:54:16 +0300

    error in 4

--- js/hw7/hw7.js
+++ js/hw7/hw7.js
@@ -37,3 +37,69 @@ switch (lang) {
   default:
     console.log("Вы говорите так быстро, что ни один переводчик вас не поймет");
 }
+
+//todo: #3
+
+const drink = "coffee";
+let price;
+switch (drink) {
+  case "tea":
+    price = 25;
+    console.log(`you have to pay ${price} grn.`);
+    break;
+  case "coffee":
+    price = 30;
+    console.log(`you have to pay ${price} grn.`);
+    break;
+  case "juice":
+    price = 20;
+    console.log(`you have to pay ${price} grn.`);
+    break;
+  default:
+    price = 0;
+    console.log("Our shop does not have this drink");
+};
+
+//todo: #4
+
+const day = prompt("What day is it today?");
+switch (day) {
+  case "Monday" || "Tuesday" || "Wednesday" || "Thursday" || "Friday":
+    console.log("Today tou have to go to your accursed work");
+    break;
+  case "Saturday" || "Sunday":
+    console.log("In these days people rest, but not you");
+    break;
+  case "31":
+    console.log("Today you will get ur salary!");
+    break;
+  default:
+    console.log("You don't obey the laws of physics");
+}
+
+//todo: #5
+
+const month = prompt("What month is it today?");
+let timeOfTheYear;
+switch (month) {
+  case "January" || "February" || "December":
+    timeOfTheYear = "winter";
+    console.log(`It is ${timeOfTheYear} now`);
+    break;
+  case "March" || "April" || "May":
+    timeOfTheYear = "spring";
+    console.log(`It is ${timeOfTheYear} now`);
+    break;
+  case "June" || "July" || "August":
+    timeOfTheYear = "summer";
+    console.log(
+      `It is ${timeOfTheYear} now. It is the best time of the year, and don't argue with me!`
+    );
+    break;
+  case "September" || "October" || "November":
+    timeOfTheYear = "autumn";
+    console.log(`It is ${timeOfTheYear} now. So, you can go to OctoberFest`);
+    break;
+  default:
+    console.log("Maybe, you live in the other planet");
+}
