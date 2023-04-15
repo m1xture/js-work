f4805cc7f7951add69270cfc20bec6157e1a9112
m1xture <109725502+m1xture@users.noreply.github.com>
2023-04-15 15:12:47 +0300

    🎵🎵🎵

--- js/hw8/hw8.js
+++ js/hw8/hw8.js
@@ -8,60 +8,58 @@
 
 const numberd = 7;
 for (let i = 1; i <= 10; i += 1) {
-    const tab = numberd * i;
-    console.log(`${numberd} * ${i} = ${tab}`);
+  const tab = numberd * i;
+  console.log(`${numberd} * ${i} = ${tab}`);
 }
 
 //todo: #2
 
-// let sum;
-// for (let i = 12; i <= 27; i += 1) {
-//     sum = i + i;
-//     console.log(sum);
-// }
+let sum = 0;
+for (let i = 12; i <= 27; i += 1) {
+    sum += i;
+    console.log(sum);
+}
 
 //todo: #3
 
 const min = 3;
 const max = 9;
-let product;
+let product = 1;
 for (let i = min; i <= max; i += 1) {
-    if (i % 2 === 0) {
-        console.log("Число парне");
-    } else {
-        product = i * i;
-        console.log(product);
-    }
+  if (i % 2 === 0) {
+    console.log("Число парне");
+  } else {
+    product *= i;
+    console.log(product);
+  }
 }
 
 //todo: #4
 
 for (let i = 1; i <= 100; i += 1) {
-    if (i === 49) {
-        console.log("49 is detected!");
-        break;
-    }
+  if (i === 49) {
+    console.log("49 is detected!");
+    break;
+  }
 }
 
 //todo: #5
 
 for (let i = 1; i <= 30; i += 1) {
-    if (i === 15) {
-        continue;
-    }
+  if (i === 15) {
+    continue;
+  }
 }
 
 //todo: #6
 
 // let summary;
-let totalPrice;
+let totalPrice = 0;
 let price = 0;
 const goods = 8;
 for (let i = 1; i <= goods; i += 1) {
-    price = Math.round(Math.random() + 5000 - 500 + 500);
-    console.log(`The price for ${i} item is ${price}`);
-    totalPrice = price + price;
-    if (i === 8) {
-        console.log(`The total price is ${totalPrice}`);
-    }
+  price = Math.round(Math.random() * (5000 - 500) + 500);
+  console.log(`The price for ${i} item is ${price}`);
+  totalPrice += price;
 }
+console.log(`The total price is ${totalPrice}`);
