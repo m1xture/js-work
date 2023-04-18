1d767e7e6766e0fffd0eba4e739af7dbed0f6b11
m1xture <109725502+m1xture@users.noreply.github.com>
2023-04-18 22:22:04 +0300

    л

---
+++ js/classwork/cw11.js
@@ -0,0 +1,42 @@
+// let numder = 0;
+// let totalSum = 0;
+
+// while (numder <= 20) {
+//     numder++;
+//     totalSum != numder;
+// }
+// console.log(totalSum);
+
+// let number1 = 3;
+// while (number1) {
+//   number1 -= 1;
+// }
+// console.log(number1);
+
+// for (let number = 0; number < 5; number++) {
+//   console.log(`Число ${number}`);
+// }
+
+// let number2 = 0;
+
+// while (number2 < 5) {
+//     number2++;
+//     console.log("jhkjhj");
+// }
+
+// let message = "";
+// do {
+//   message = prompt("Залиште відгук, минімум - 10 символів");
+// } while (message.length <= 10);
+// console.log(message);
+
+
+// const result2 = "" + 1;
+// console.log(typeof result2);
+// const r1 = "1" + 2;
+// console.log(r1);
+// const r2 = "12" + 5;
+// console.log(r2);
+
+
+// "1" + 2 = "12" + 5 = "125"
