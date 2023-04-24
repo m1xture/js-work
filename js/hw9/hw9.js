727f4f0e445817c3cd3b797b6d64059955d3feec
m1xture <109725502+m1xture@users.noreply.github.com>
2023-04-24 14:29:15 +0300

    я старался

--- js/hw9/hw9.js
+++ js/hw9/hw9.js
@@ -1,14 +1,29 @@
 //todo: #1
 
-const nums = [1, 3, 67];
-nums[2] = 10;
-console.log(nums);
+// const nums = [1, 3, 67];
+// nums[2] = 10;
+// console.log(nums);
 
-//todo: #2
+// //todo: #2
 
-const strs = ["jfhdj", "dsdf", "retrhm"];
-strs.push("fgdfg");
-console.log(strs);
+// const strs = ["jfhdj", "dsdf", "retrhm"];
+// strs.push("fgdfg");
+// console.log(strs);
 
 //todo: #3
 
+// const arrs = [];
+// let arrSum;
+// for (let i = 1; i <= 7; i += 1) {
+//     const arr = Number(Math.round(Math.random() * (200 - 1) + 1));
+//     arrs.push(arr);
+//     arrSum += arr;
+// }
+// console.log(arrSum);
+
+//todo: #4
+
+const array2 = [4, 5, 8, 3, 2];
+for (let i = 1; i <= 6; i += 1) {
+    console.log(array2[i]);
+}
