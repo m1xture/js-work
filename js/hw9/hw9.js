35fced6909435ba44f4285f0327d1dab5182f8ab
m1xture <109725502+m1xture@users.noreply.github.com>
2023-04-29 16:16:54 +0300

    Update hw9.js

--- js/hw9/hw9.js
+++ js/hw9/hw9.js
@@ -24,7 +24,7 @@ console.log(arrSum);
 //todo: #4 right
 
 const array2 = [4, 5, 8, 3, 2];
-for (let i = 0; i <= 4; i += 1) {
+for (let i = 0; i <= array2.length; i += 1) {
   console.log(array2[i]);
 }
 
@@ -47,6 +47,7 @@ for (let i = 0; i < arrayStr.length; i += 1) {
 const arrayNumber = [
   939, 3737, 13356, 432423, 0, -48, 38, 122222, 7897329385, -87912313,
 ];
+  
 const arrayMax = Math.max.apply(null, arrayNumber);
 console.log(arrayMax);
 
@@ -58,4 +59,39 @@ for (let i = 1; i <= arrNumLenght; i += 1) {
   if (arrNum[i] % 2 === 0) {
     console.log(arrNum[i]);
   }
+}
+
+//todo: #8
+
+const question = prompt("What country are you from?");
+const NormalAnswer = question.toLowerCase();
+const china = "китай";
+const jamaica = "ямайка";
+const chile = "чилі";
+const australia = "австралія";
+const india = "індія";
+let price;
+switch (NormalAnswer) {
+  case china:
+    price = 100;
+    console.log(`Доставка в ${china} буде коштувати ${price} кредитів`);
+    break;
+  case jamaica:
+    price = 120;
+    console.log(`Доставка в ${jamaica} буде коштувати ${price} кредитів`);
+    break;
+  case chile:
+    price = 250;
+    console.log(`Доставка в ${chile} буде коштувати ${price} кредитів`);
+    break;
+  case australia:
+    price = 170;
+    console.log(`Доставка в ${australia} буде коштувати ${price} кредитів`);
+    break;
+  case india:
+    price = 80;
+    console.log(`Доставка в ${india} буде коштувати ${price} кредитів`);
+    break;
+  default:
+    console.log("У вашій країні доставка недоступна");
 }
