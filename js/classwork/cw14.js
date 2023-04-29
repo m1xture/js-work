db276776bb2be6e1b90e8881209f3518738ca241
m1xture <109725502+m1xture@users.noreply.github.com>
2023-04-29 15:08:32 +0300

    ъ

--- js/classwork/cw14.js
+++ js/classwork/cw14.js
@@ -22,33 +22,42 @@ let sum = 0;
 
 //todo: #1
 
-for (let element of arr) {
-  if (element % 2 !== 0) {
-      sum += element;
-      console.log(element);
-  }
-}
-console.log(sum);
+// for (let element of arr) {
+//   if (element % 2 !== 0) {
+//       sum += element;
+//       console.log(element);
+//   }
+// }
+// console.log(sum);
 
-//todo: #2
+// //todo: #2
 
-for (let element2 of arr) {
-    if (element2 % 2 === 0) {
-        continue;
-    }
-        sum += element2;
-        console.log(element2);
-}
-console.log(sum);
+// for (let element2 of arr) {
+//     if (element2 % 2 === 0) {
+//         continue;
+//     }
+//         sum += element2;
+//         console.log(element2);
+// }
+// console.log(sum);
 
-//todo: #3
+// //todo: #3
 
-for (let element3 of arr) {
-    if (element3 === 324) {
-        console.log("we found it!" + " " + element3);
-        break;
-    }
-}
+// for (let element3 of arr) {
+//     if (element3 === 324) {
+//         console.log("we found it!" + " " + element3);
+//         break;
+//     }
+// }
 
 //todo: #4
 
+// let numb = 35;
+// if (numb < 0) {
+//     console.log("ok");
+// } else if ( numb >= 0) {
+//     numb = -numb;
+//     console.log(numb);
+//     const sum = numb + 1;
+//     console.log(sum);
+// }
