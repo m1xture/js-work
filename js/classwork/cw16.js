d39c1952a6c99d77088ed7041a3df93ad7ef6f7d
m1xture <109725502+m1xture@users.noreply.github.com>
2023-05-04 19:45:45 +0300

    h

---
+++ js/classwork/cw16.js
@@ -0,0 +1,10 @@
+//todo: Напиши скрипт, який змінює регістр
+//todo: кожного символу в рядкуна протилежний.
+//todo: Наприклад, якщо рядок «JavaScript»,
+//todo: то в результаті буде рядок «jAVAsCRIPT»
+
+const word = "JavaScript";
+const letters = word.split();
+for (let i = 0; i < letters.length; i += 1) {
+    // console.log(letters[i]);
+}
