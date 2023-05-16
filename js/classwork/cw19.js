c8f3e5b2cfaffc15fb1143e3dcd8d0377b5eade8
m1xture <misha.ivanov.1050@gmail.com>
2023-05-16 19:09:58 +0300

    р

---
+++ js/classwork/cw19.js
@@ -0,0 +1,11 @@
+function showInfo (x, y) {
+    console.log(x);
+    console.log(y);
+};
+showInfo(2, 10);
+const showInfo = function (x, y) {
+    console.log(x);
+    console.log(y);
+};
+showInfo(2, 10);
+const result = showInfo(2, 10);
