3cd47b73a9957c3f5889a60799cc78921828c788
m1xture <misha.ivanov.1050@gmail.com>
2023-05-29 12:20:50 +0300

    🤷‍♂️

--- js/hw12/hw12.js
+++ js/hw12/hw12.js
@@ -1,8 +1,8 @@
 //todo: #1
 
-function makePizza () {
-    return "Your pizza is being prepared, please wait.";
-  };  
+function makePizza() {
+  return "Your pizza is being prepared, please wait.";
+};
 const result = makePizza();
 const pointer = makePizza;
 
@@ -28,7 +28,7 @@ console.log(res);
 
 //todo: #3
 
-const res1 = madePizza("Ultracheese", function eatPizza (pizzaName) {
+const res1 = madePizza("Ultracheese", function eatPizza(pizzaName) {
   return `Eating pizza ${pizzaName}`;
 });
 
@@ -40,7 +40,7 @@ const btnRef = document.querySelector("[data-hero-btn]");
 const resultRef = document.querySelector("[data-result]");
 let result1 = "Hello world!";
 
-function writeAnswer () {
+function writeAnswer() {
   // const resultValue = result1.value;
   resultRef.textContent = result1;
 };
@@ -53,9 +53,37 @@ const clickedBtn = document.querySelector("button");
 const strNums = document.querySelector("[data-nums");
 let clicks = 0;
 
-function writeNums () {
+function writeNums() {
   clicks += 1;
   strNums.textContent = clicks;
 }
 
 clickedBtn.addEventListener("click", writeNums);
+
+//todo: #6
+//? на соплях
+
+const applyCallbackToEachElement = (arr, callback) => {
+  return squareCallback(arr);
+};
+const arr = [1, 2, 3, 4, 5];
+const squareCallback = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] *= arr[i]
+  };
+  return arr;
+};
+const result2 = applyCallbackToEachElement(arr, squareCallback);
+console.log(result2); // [1, 4, 9, 16, 25]
+
+//todo: #7
+
+const calculateDiscountedPrice = (price, discount, callback) => {
+  const discountedPrice = price - discount;
+  callback(discountedPrice);
+};
+const showDiscountedPrice = (discountedPrice) => {
+  console.log(discountedPrice);
+  return discountedPrice;
+};
+calculateDiscountedPrice(1000, 10, showDiscountedPrice); // Discounted price: 990
