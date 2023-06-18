d16e8aef84313752a10d947444bdd34a1ebd2138
m1xture <109725502+m1xture@users.noreply.github.com>
2023-06-18 16:58:50 +0300

    la-la-la

--- js/classwork/cw26.js
+++ js/classwork/cw26.js
@@ -1,5 +1,39 @@
 //? ссылочный тип {} === {}
 
-const 
+// const testObj = {
+//   name: "Senya",
+//   age: 13,
+// };
 
-console.log({a: 15} === {a: 15});
+// const b = testObj;
+
+// console.log(testObj === b);
+
+// console.log({ a: 15 } === { a: 15 });
+
+
+// const a = [987, ];
+// const b = a;
+// console.log(a === b);
+// a.hello = "flflflf";
+// console.log(a);
+
+// const f = num => {
+//     console.log(num);
+// };
+
+// const f2 = f;
+
+// console.log(f === f2);
+
+// f.hello = 'llslslslsllalala';
+// console.dir(f);
+
+
+const a = [1, 3, 2, 4, 5];
+a.push(6);
+console.log(a);
+
+//todo: object's methods
+
+//? this.key
