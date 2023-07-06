9b9c5c29231b93374dbc96079662acf266bf6c83
m1xture <109725502+m1xture@users.noreply.github.com>
2023-07-06 20:29:52 +0300

    good

---
+++ js/classwork/cw32.js
@@ -0,0 +1,20 @@
+// const userProfile = {
+//     userName: "tututtu",
+//     userAvatar: "./imgs/tututtu.png",
+//     userLocation: "Ukraine",
+//     userStats: {
+//         followers: 12,
+//         views: 1427,
+//         likes: 10,
+//     },
+//     userAchievements: ["HTML 5", "CSS 3", "SASS", "ES 6", "React"],
+// };
+// const { userName, userAvatar, userLocation, userStats: {followers, views, likes} } = userProfile;
+// console.log(followers);
+
+//todo:
+
+const friends = ["Vitaliy", "Yakov", "Ruslan", "met25", "Semen", "Illya", "Michael"];
+const [friend1, friend2, friend3, friend4, friend5, friend6, friend7, frieend8 = "lalala"] = friends;
+console.log(friend4);
+console.log(frieend8);
