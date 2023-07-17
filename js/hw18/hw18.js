3c80ff6aa25aff26ef245e864c8c46811e5c3716
m1xture <109725502+m1xture@users.noreply.github.com>
2023-07-17 13:14:02 +0300

    🎶🎧

--- js/hw18/hw18.js
+++ js/hw18/hw18.js
@@ -7,7 +7,7 @@ const findNames = (coll) => {
 
 const nnms = [{ name: "lalala" }, { name: "kls" }, { name: "ksdbh" }];
 
-findNames(nnms)
+findNames(nnms);
 
 //todo: Отримати масив об'єктів користувачів за кольором очей (поле eyeColor).
 
@@ -46,3 +46,45 @@ const wqer = [
 ];
 
 findSomePeople(wqer, "male");
+
+//todo: #4 isActive
+
+const findInactivePeople = (array) => {
+  const inactivePeople = array.filter((person) => person.isActive === false);
+  console.log(inactivePeople);
+  return inactivePeople;
+};
+
+const people = [{ isActive: false }, { isActive: true }, { isActive: false }];
+
+findInactivePeople(people);
+
+//todo: #5 Отримати користувача (не масив) по email (поле email, він унікальний).
+
+const users = [
+  { name: "Vanya", email: "ivan@gamail.com", age: 15 },
+  { name: "John", email: "lalala@gmal.com", age: 65 },
+  { name: "KOlya", email: "doctor12345@mail.com", age: 7 },
+  { name: "met25", email: "matvii.savula@gmail.com", age: 13 },
+];
+
+const findSomeone = (array, email) => {
+  const foundPerson = array.find((person) => person.email === email);
+  console.log(foundPerson);
+  return foundPerson;
+};
+
+findSomeone(users, "doctor12345@mail.com");
+
+//todo: #6 Отримати масив користувачів, які потрапляють у
+//todo: вікову категорію від min до max років(поле age).
+
+const findPeopleByAge = (arrayOfUsers, minAge, maxAge) => {
+  const foundPeopleByAge = arrayOfUsers.filter(
+    person => person.age > minAge && person.age < maxAge
+  );
+  console.log(foundPeopleByAge);
+  return foundPeopleByAge;
+};
+
+findPeopleByAge(users, 50, 90);
