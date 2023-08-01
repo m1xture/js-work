6ec62809c9b3884255f5ed0d981b34014d5dccbb
m1xture <109725502+m1xture@users.noreply.github.com>
2023-08-01 11:49:41 +0300

    mkl

--- js/hw20/hw20.js
+++ js/hw20/hw20.js
@@ -1,8 +1,8 @@
 const users = [
-  { balance: 1000, friend: "Dog", name: "Jason" },
-  { balance: 10, friend: "Vova", name: "Jack" },
-  { balance: 1429, friend: "", name: "John" },
-  { balance: 500, friend: "Anya", name: "Json" },
+  { balance: 1000, friend: ["lalala", "hahaha", "qwerty42"], name: "Jason", skills: ["writing"]},
+  { balance: 10, friend: ["Kolya", "Mark"], name: "Jack", skills: ["soft skills"] },
+  { balance: 1429, friend: ["Tolya"], name: "John", skills: ["Python language"] },
+  { balance: 500, friend: ["Dog"], name: "Json", skills: ["json files"] },
 ];
 
 //todo: Отримати загальну суму балансу (поле balance) всіх користувачів.
@@ -12,19 +12,20 @@ console.log(totalBalance);
 
 //todo: Масив імен всіх користувачів у яких є друг із зазначеним ім'ям.
 
-const findUsersWithFriend = (array, friend) => {
-  const usersWithThatFriend = array.reduce((acc, user) => {
-    if (user.friend === friend) {
-      console.log(acc);
-      return acc.push(user.name);
-    } else {
-      return acc;
-    }
-  }, []);
-};
-
-console.log(findUsersWithFriend(users, "Dog"));
+let namesOfFriends = [];
+const arrOfNames = users.reduce((acc, user) => {
+  if (user.friend.find((friend) => friend === "Dog")) {
+    return acc.push(user.name);
+  };
+  return acc;
+}, []);
+console.log(arrOfNames);
 
 //todo: Масив імен (поле name) людей, відсортованих в залежності від кількості їх друзів (поле friends).
 
+const sortedUsers = users.sort((u1, u2) => u1.friend.length - u2.friend.length);
+console.log(sortedUsers);
+console.log(users);
 //todo: Отримати масив всіх умінь всіх користувачів (поле skills), при цьому не має бути повторюваних умінь і вони повинні бути відсортовані в алфавітному порядку.
+
+// const skillsUsers = users.sort((u1, u2) => );
