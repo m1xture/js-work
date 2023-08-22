ce689ff41f830d24113c94b9308d429e9f083010
m1xture <109725502+m1xture@users.noreply.github.com>
2023-08-22 20:55:17 +0300

    jhh

--- js/classwork/cw40.js
+++ js/classwork/cw40.js
@@ -1,30 +1,68 @@
 //todo: met25
 
-
 const friend = {
-    group: "FE-2",
-    school: "Goiteens",
-    teacher: "Tate-T"
+  group: "FE-2",
+  school: "Goiteens",
+  teacher: "Tate-T",
 };
 
 const firstFriend = Object.create(friend);
 
-// console.log(firstFriend.group);
+// // console.log(firstFriend.group);
 
-firstFriend.friendName = 'met25';
-// console.log(firstFriend);
-
+firstFriend.friendName = "met25";
+// // console.log(firstFriend);
 
 //? #2 friend
 
 const secondFriend = Object.create(friend);
 
 secondFriend.friendName = "Senya";
-// console.log(secondFriend);
+// // console.log(secondFriend);
 secondFriend.showInfo = function () {
-    return this.friendName;
-}
+  return this.friendName;
+};
 
 const senyasClone = Object.create(secondFriend);
 
-console.log(senyasClone.showInfo());
+//todo: Senya26
+
+//? Алгоритм поиска свойства в цепочке прототипов
+//? Поиск начинается в собственых свойствах объекта.
+//? Если ключа в собствнных свойствах нет, тогда поиск переход к цепочке прототипов
+//* Как только находит нужное свойство, прекращает поиск
+
+// console.log(friend.age);
+
+
+//todo: Фунции конструкторы
+
+
+// const Car = function (brand, model, price) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+// };
+
+// const ferrariF50 = new Car("ford", "Fiesta", 5000);
+// console.log(ferrariF50);
+
+
+const Car = function ({brand = "", model = "", price = 0}) {
+  this.brand = brand;
+  this.model = model;
+  this.price = price;
+};
+
+Car.prototype.showInfo = function (newBrand) {
+    this.brand = newBrand;
+  return `${model}`;
+};
+
+const newgh = new Car({brand: "lamborgini"});
+newgh.color = "violet";
+newgh.year = 2220
+console.log(newgh);
+
+newgh.showInfo("newjdfgns");
+
