10171680f8b624341a04e93565f2873ebb9ded8b
m1xture <109725502+m1xture@users.noreply.github.com>
2023-09-05 21:17:57 +0300

    h

--- js/classwork/cw43.js
+++ js/classwork/cw43.js
@@ -19,23 +19,100 @@
 // console.log(toyota.brand);
 // console.log(toyota.brand = "yoyoyo");
 
-class Hero {
-  constructor({ heroName = "", heroScores = 0 }) {
-    this.name = heroName;
-    this.scores = heroScores;
+// class Hero {
+//   constructor({ heroName = "", heroScores = 0 }) {
+//     this.name = heroName;
+//     this.scores = heroScores;
+//   }
+// }
+
+// class Orks extends Hero {
+//   constructor({ skinColor = "" }) {
+//     super(name, scores);
+//     this.skinColor = skinColor;
+//   }
+// }
+
+// class Elfs extends Hero {
+//   constructor({ earsLength }) {
+//     super(name, scores);
+//     this.earsLength = earsLength;
+//   }
+// }
+
+//todo: Classes met25
+
+class Animal {
+  // constructor(animalName) {
+  //   this.animalName = animalName;
+  // }
+  makeSound() {
+    console.log("Roaaaaar");
   }
 }
 
-class Orks extends Hero {
-  constructor({ skinColor = "" }) {
-    super(name, scores);
-    this.skinColor = skinColor;
+// class Dog extends Animal {
+//   constructor({ breed = "" }) {
+//     super(animalName);
+//     this.breed = breed;
+//   }
+// }
+
+//todo: Classes Yakov29
+
+// class Cat extends Animal {
+//   makeSound() {
+//     console.log("nya");
+//   }
+// }
+
+// const animal = new Animal();
+// animal.makeSound(); // "The animal makes a sound"
+// const cat = new Cat();
+// cat.makeSound(); // "Meow"
+
+// //todo: me
+
+// class Shape {
+//   constructor({ color = "" }) {
+//     this.color = color;
+//   }
+//   draw() {
+//     console.log(`Drawing ${this.color} shape`);
+//   }
+// }
+
+// class Circle extends Shape {
+//   constructor({ color = "", radius = 0 }) {
+//     super({ color });
+//     this.radius = radius;
+//   }
+//   draw() {
+//     console.log(`Drawing ${this.color} circle with radius ${this.radius}`);
+//   }
+// }
+
+// const myCircle = new Circle({ color: "red", radius: 6 });
+// myCircle.draw(); // Drawing a circle with radius 5
+// console.log(myCircle.color); // "red"
+
+class Person {
+  constructor( name = "", age = 0 ) {
+    this.name = name;
+    this.age = age;
   }
 }
 
-class Elfs extends Hero {
-  constructor({ earsLength }) {
-    super(name, scores);
-    this.earsLength = earsLength;
+class Employee extends Person {
+  constructor( name = "", age = 0, position = "" ) {
+    super(name, age);
+    this.position = position;
+  }
+  getInfo() {
+    console.log(
+      `${this.name} is a ${this.position} and he is ${this.age} y.o.`
+    );
   }
 }
+const john = new Employee ("John", 32, "programmer" );
+john.getInfo(); // "John is a programmer and is 32 years old"
