504c624efb567372aa96813d13643b4ba8f82675
m1xture <109725502+m1xture@users.noreply.github.com>
2023-09-09 16:05:48 +0300

    ;l

--- js/theme.js
+++ js/theme.js
@@ -1,3 +1,11 @@
 document.querySelector(".input--theme").addEventListener("click", () => {
-    document.body.classList.toggle("isDark");
+  document.body.classList.toggle("isDark");
 });
+
+let clicks = 0;
+window.addEventListener("click", () => {
+  clicks++;
+  document.querySelector("[data-text]").textContent = clicks;
+});
+
+
