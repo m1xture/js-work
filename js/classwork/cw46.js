4c5c50710acc320a7dada71f6392fc2139cd29c4
m1xture <109725502+m1xture@users.noreply.github.com>
2023-09-14 21:23:18 +0300

    n

---
+++ js/classwork/cw46.js
@@ -0,0 +1,10 @@
+// const listRef = document.querySelector(".site-nav");
+
+// listRef.style.backgroundColor = "orange";
+// listRef.children[0].children[0].classList.add("site-nav__link");
+
+//todo: #2
+
+
+const listRef = document.querySelector("[name=list]");
+listRef.computedStyleMap.color = "red";
