6ce72a24586dc6d2d5bc6393797c112ca39e3a72
m1xture <109725502+m1xture@users.noreply.github.com>
2023-09-15 20:54:25 +0300

    m

---
+++ js/theme.js
@@ -0,0 +1,27 @@
+const refs = {
+  openBtn: document.querySelector("[data-modal-open]"),
+  closeBtn: document.querySelector("[data-modal-close]"),
+  backdropElem: document.querySelector("[data-modal-backdrop]"),
+};
+
+const { openBtn, closeBtn, backdropElem } = refs;
+
+openBtn.addEventListener("click", function () {
+  backdropElem.classList.toggle("is-hidden");
+});
+
+backdropElem.addEventListener("click", function (e) {
+  if (e.target === document.querySelector(".modal")) {
+    return;
+  }
+  if (e.target === closeBtn) {
+    console.log(e.target);
+    this.classList.toggle("is-hidden");
+  }
+  if (e.target === backdropElem) {
+    console.log(e.target);
+    this.classList.toggle("is-hidden");
+  }
+  console.log(e.target);
+  console.log(this);
+});
