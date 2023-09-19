32398fc89e3044f8ea890e16f6b84714f57e0e7b
m1xture <109725502+m1xture@users.noreply.github.com>
2023-09-19 21:13:57 +0300

    km

--- js/classwork/cw46.js
+++ js/classwork/cw46.js
@@ -5,6 +5,46 @@
 
 //todo: #2
 
+const linkRef = document.querySelector("[name=list]");
 
-const listRef = document.querySelector("[name=list]");
-listRef.computedStyleMap.color = "red";
+linkRef.style.color = 'orange';
+
+//? #2
+
+const listRef = document.querySelectorAll("li");
+console.log(listRef);
+
+listRef[0].firstElementChild.classList.add("req9");
+listRef[listRef.length -1].classList.add("req9");
+
+//todo: #3
+
+const titleEl = document.querySelector("h1");
+
+titleEl.classList.add("req9");
+titleEl.textContent = "Titiitle";
+
+titleEl.classList.replace("req9", "titile");
+
+console.log(titleEl.classList.contains("list"));
+
+//todo: #3 true
+
+const tableRef = document.getElementById("age-table");
+
+// console.log(tableRef);
+
+const labelsRefs = tableRef.querySelectorAll("label");
+console.log(labelsRefs);
+const firstTdRef = tableRef.querySelector("td");
+console.log(firstTdRef);
+
+const formEl = document.querySelector("form[name=search-person]");
+console.log(formEl);
+
+const firstInputEl = formEl.querySelector("input");
+console.log(firstInputEl);
+
+const inputsEls = formEl.querySelectorAll("input");
+const lastInp = inputsEls[inputsEls.length - 1];
+console.log(lastInp);
