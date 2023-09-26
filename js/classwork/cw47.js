2039e90ec8b663954b306c1b263915c0ca1a3809
m1xture <109725502+m1xture@users.noreply.github.com>
2023-09-26 21:58:30 +0300

    fg

---
+++ js/classwork/cw47.js
@@ -0,0 +1,24 @@
+// const boxEl = document.querySelector("[data-box-elem]");
+// console.log(boxEl.innerHTML);
+
+// // boxEl.innerHTML = boxEl.innerHTML + `<article>Lalala</article>`;
+
+// boxEl.insertAdjacentHTML("beforeend", `<span>gdfgfgdfd</span>`);
+
+
+const inputsEl = document.querySelectorAll('[data-focus]');
+
+function focusChange () {
+    if (inputsEl[0].dataset.focus === "save") {
+        console.log("dlf");
+        return;
+    } else {
+        console.log("me");
+    }
+}
+
+focusChange();
+console.log(inputsEl[0].dataset);
+
+//# dfsdfsdfsds
+//desc fs
