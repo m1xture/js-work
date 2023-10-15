ddbc1017de84545b2d6c95195fbe5d9f760b17ce
m1xture <109725502+m1xture@users.noreply.github.com>
2023-10-15 20:43:41 +0300

    hw28

--- js/hw28/hw28.js
+++ js/hw28/hw28.js
@@ -22,12 +22,86 @@ document.addEventListener("keydown", (e) => {
     galleryItems[i].classList.add("visible");
     i++;
   }
+  // console.log(e.code);
   if (e.code === "ArrowLeft") {
-    if (i === 0) {
-      i = 1;
+    if (i - 1 < 0) {
+      galleryItems[i].classList.remove("visible");
+
+      i = galleryItems.length - 1;
     }
+    // i = 1;
     galleryItems[i].classList.remove("visible");
     galleryItems[i - 1].classList.add("visible");
     i--;
   }
 });
+
+//todo: #2
+
+let redFilter = 0;
+let greenFilter = 0;
+let blueFilter = 0;
+let bgColor = "";
+
+function createRandomBg() {
+  redFilter = Math.round(Math.random() * 255 - 0);
+  greenFilter = Math.round(Math.random() * 255 - 0);
+  blueFilter = Math.round(Math.random() * 255 - 0);
+  bgColor = `rgb(${redFilter}, ${greenFilter}, ${blueFilter})`;
+  return bgColor;
+}
+
+let elemWidth = 30;
+let elemHeight = 30;
+
+const refs = {
+  inputAmountEl: document.querySelector("[data-amount]"),
+  btnCreateEl: document.querySelector("[data-action=render]"),
+  btnClearEl: document.querySelector("[data-action=destroy]"),
+  itemsBlockEl: document.querySelector(".boxes"),
+};
+
+const { inputAmountEl, btnClearEl, btnCreateEl, itemsBlockEl } = refs;
+btnCreateEl.addEventListener("click", (e) => {
+  for (let i = 0; i < inputAmountEl.value; i++) {
+    console.log(inputAmountEl.value);
+    itemsBlockEl.insertAdjacentHTML("beforeend", "<li class=box></li>");
+  }
+  console.log(document.querySelectorAll(".box"));
+  document.querySelector("[data-error]").textContent = "";
+  console.log(refs.itemsBlockEl.innerHTML);
+  if (!refs.itemsBlockEl.innerHTML) {
+    elemHeight = 30;
+    elemWidth = 30;
+  }
+
+  document.querySelectorAll(".box").forEach((item) => {
+    item.style.backgroundColor = createRandomBg();
+    item.style.width = `${elemWidth}px`;
+    item.style.height = `${elemHeight}px`;
+    elemWidth += 10;
+    elemHeight += 10;
+    if (elemHeight > 300) {
+      elemHeight = 30;
+      elemWidth = 30;
+    }
+  });
+  // document.querySelector('.boxes').style.display = "flex";
+});
+
+function clearItems() {
+  // console.log(refs.itemsBlockEl.innerHTML);
+  if (refs.itemsBlockEl.innerHTML) {
+    refs.itemsBlockEl.innerHTML = "";
+  } else {
+    document.querySelector("[data-error]").textContent =
+      "There are not any items";
+  }
+  if (refs.itemsBlockEl.innerHTML === "") {
+    elemHeight = 30;
+    elemWidth = 30;
+  }
+}
+refs.btnClearEl.addEventListener("click", clearItems);
+
+// console.log(refs.btnCreateEl);
