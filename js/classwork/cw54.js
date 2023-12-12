3bdc9f8f798789d3586722de3b65e0be1fd021f1
m1xture <109725502+m1xture@users.noreply.github.com>
2023-12-12 21:49:05 +0200

    dsgfd

---
+++ js/classwork/cw54.js
@@ -0,0 +1,29 @@
+const targetElem = document.querySelector(".target");
+
+document.addEventListener("scroll", () => {
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.1,
+  };
+
+  function callbackFn(entries, observer) {
+    console.log("cb call");
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        //   console.log(true);
+        targetElem.classList.add("visible");
+      }
+      //   else {
+      //     targetElem.classList.remove("visible");
+      //   }
+      if (!entry.isIntersecting) {
+        targetElem.classList.remove("visible");
+      }
+    });
+  }
+
+  const observer = new IntersectionObserver(callbackFn, options);
+
+  observer.observe(targetElem);
+});
