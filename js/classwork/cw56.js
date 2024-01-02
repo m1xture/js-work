51249716dd9dfced7ca00f9990871f7881ee340f
m1xture <109725502+m1xture@users.noreply.github.com>
2024-01-02 20:44:15 +0200

    hjmbj

--- js/classwork/cw56.js
+++ js/classwork/cw56.js
@@ -31,24 +31,50 @@
 // };
 // xhr.send(null);
 
-const divEl = document.createElement("div");
-divEl.style.backgroundColor = "red";
-document.body.append(divEl);
+// const divEl = document.createElement("div");
+// divEl.style.backgroundColor = "red";
+// document.body.append(divEl);
 
-function showDiv() {
-  objArr.forEach(({ hex }) => {
-    const div = document.createElement("div");
-    div.style.backgroundColor = HEX;
-    document.body.append(div);
-  });
-}
+// function showDiv() {
+//   objArr.forEach(({ hex }) => {
+//     const div = document.createElement("div");
+//     div.style.backgroundColor = HEX;
+//     document.body.append(div);
+//   });
+// }
 
-fetch("https://pokeapi.co/api/v2/pokemon/ditto")
-  .then((res) => {
-    console.log(res);
-    return res.json();
-  })
-  .then((res) => {
-          console.log(res);
-          console.log(res);
-  });
+// fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+//   .then((res) => {
+//     console.log(res);
+//     return res.json();
+//   })
+//   .then((res) => {
+//     console.log(res);
+//     console.log(res);
+//   });
+
+// 1. Створіть об'єкт JSON з інформацією про книгу. Книга повинна мати наступні поля:
+
+// назва книги
+// автор книги
+// рік видання
+// жанр
+
+// ☝️Перевірте, що ваш JSON є коректним за допомогою методу JSON.parse().
+
+// 2. Запросіть у вашого користувача ім'я та вік за допомогою input.
+// Створіть об'єкт JSON з інформацією про користувача,
+// використовуючи дані, які ви отримали від користувача.
+// Перевірте, що ваш JSON є коректним за допомогою методу JSON.parse().
+
+// const str = `{"title": "Harry Poter", "author": "J. Rowling", "year": 1997, "genre": "fantasy"}`;
+// console.log(JSON.parse(str));
+
+const formEl = document.querySelector("form");
+
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const str = `{ "name": "${e.currentTarget.fname.value}", "age": ${e.currentTarget.age.value} }`;
+  console.log(str);
+  console.log(JSON.parse(str));
+});
