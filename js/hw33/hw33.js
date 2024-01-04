3284465c7193abac2104209f835716d2ea234684
m1xture <109725502+m1xture@users.noreply.github.com>
2024-01-04 21:33:33 +0200

    h

--- js/hw33/hw33.js
+++ js/hw33/hw33.js
@@ -1,7 +1,27 @@
-const fs = require('fs');
 const formElem = document.querySelector("form");
+
+function addSidebar() {
+  return (document.body.innerHTML =
+    "<button type=button class=sidebar__btn>Delete all info about me</button><button type=button>View info about me</button>");
+}
+
 formElem.addEventListener("submit", (e) => {
   e.preventDefault();
-  const fileContent = fs.readFileSync(filePath, "utf-8");
-  console.log(fileContent);
+  const obj = {
+    firstName: e.currentTarget.fname.value,
+    lastName: e.currentTarget.lname.value,
+    course: e.currentTarget.course.value,
+    faculty: e.currentTarget.faculty.value,
+    courses: e.currentTarget.courses.value.split(" "),
+  };
+  let arr = [];
+  if (localStorage.length !== 0) {
+    arr = [localStorage.getItem("student"), JSON.stringify(obj)];
+  } else {
+    arr = [JSON.stringify(obj)];
+  }
+
+  // console.log(obj.courses);
+  localStorage.setItem("student", arr);
+  formElem.classList.add("is-hidden");
 });
