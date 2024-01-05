b292d279bad6344d2401f18f4d97303e89de5027
m1xture <109725502+m1xture@users.noreply.github.com>
2024-01-05 21:13:35 +0200

    dgf

--- js/hw33/hw33.js
+++ js/hw33/hw33.js
@@ -10,18 +10,58 @@ formElem.addEventListener("submit", (e) => {
   const obj = {
     firstName: e.currentTarget.fname.value,
     lastName: e.currentTarget.lname.value,
+    age: e.currentTarget.age.value,
     course: e.currentTarget.course.value,
     faculty: e.currentTarget.faculty.value,
     courses: e.currentTarget.courses.value.split(" "),
   };
   let arr = [];
   if (localStorage.length !== 0) {
-    arr = [localStorage.getItem("student"), JSON.stringify(obj)];
+    // arr = [localStorage.getItem("student"), JSON.stringify([obj])];
+    const parsedArr = JSON.parse(localStorage.getItem("student"));
+    console.log(parsedArr);
+    parsedArr.push(obj);
+    arr = parsedArr;
+    console.log(arr);
+    console.log(parsedArr);
   } else {
-    arr = [JSON.stringify(obj)];
+    // arr = [JSON.stringify(obj)];
+    arr = [obj];
   }
 
-  // console.log(obj.courses);
-  localStorage.setItem("student", arr);
+  console.log(arr);
+  localStorage.setItem("student", JSON.stringify(arr));
   formElem.classList.add("is-hidden");
+  document.body.style.backgroundColor = "#fff";
+  document.querySelector("table").classList.add("w90");
+  document.querySelector("#table").innerHTML = `<thead>
+  <tr>
+    <th>Name</th>
+    <th>Last name</th>
+    <th>Age</th>
+    <th>Course</th>
+    <th>Faculty</th>
+    <th>Courses</th>
+  </tr>
+  </thead>
+  <tbody></tbody>
+  `;
+
+  arr.map((elem) => {
+    document.querySelector("tbody").insertAdjacentHTML(
+      "afterbegin",
+      `<tr>
+    <td>${elem.firstName}</td>
+    <td>${elem.lastName}</td>
+    <td>${elem.age}</td>
+    <td>${elem.course}</td>
+    <td>${elem.faculty}</td>
+    <td>${elem.courses}</td>
+    </tr>`
+    );
+  });
+
+  // console.log(JSON.parse(localStorage.getItem("student")));
 });
+
+// localStorage.setItem("arr", JSON.stringify(["lala", 2, null]));
