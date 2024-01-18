bd8f0819213fa6b572d950416ea85bcaae8aa0c2
m1xture <109725502+m1xture@users.noreply.github.com>
2024-01-18 22:01:45 +0200

    ik

--- js/classwork/cw58.js
+++ js/classwork/cw58.js
@@ -1,12 +1,23 @@
-const db = {
+// const db = {
+//   title: "Webstudio",
+//   items: ["Studio", "Porfolio", "Contacts"],
+// };
+
+// const source = document.querySelector("#menu-template").innerHTML.trim();
+// const template = Handlebars.compile(source);
+
+// const markup = template(db);
+// console.log(markup);
+
+// document.querySelector("[data-header]").innerHTML = markup;
+
+import Handlebars from "../../node_modules/handlebars";
+import template from "../template/menuTemplate.hbs";
+
+const menuData = {
   title: "Webstudio",
   items: ["Studio", "Porfolio", "Contacts"],
 };
 
-const source = document.querySelector("#menu-template").innerHTML.trim();
-const template = Handlebars.compile(source);
-
-const markup = template(db);
-console.log(markup);
-
-document.querySelector("[data-header]").innerHTML = markup;
+const markup = template(menuData);
+document.querySelector('[data-header]').innerHTML = markup;
