ba78371191bb44e28859925212a8fe21e4e266c9
m1xture <109725502+m1xture@users.noreply.github.com>
2024-01-29 20:01:27 +0200

    part1

--- js/hw35/hw35.js
+++ js/hw35/hw35.js
@@ -3,9 +3,18 @@ const refs = {
   addCardFormElem: document.querySelector("[data-add-form]"),
   todoListEl: document.querySelector("[data-todo]"),
   completedListEl: document.querySelector("[data-completed]"),
+  savedListEl: document.querySelector("[data-saved-list]"),
+  savedFormEl: document.querySelector("[data-saved-form]"),
 };
 
-const { formElem, addCardFormElem, todoListEl, completedListEl } = refs;
+const {
+  formElem,
+  addCardFormElem,
+  todoListEl,
+  completedListEl,
+  savedListEl,
+  savedFormEl,
+} = refs;
 
 function register() {
   if (formElem.login.value.length > 3 && formElem.password.value.length >= 5) {
@@ -14,6 +23,7 @@ function register() {
     console.log("You registered a new account successfully!🎊");
     formElem.parentElement.classList.add("is-hidden");
     sessionStorage.setItem("logged", JSON.stringify(true));
+    renderAllCards();
   } else {
     console.log("Too small login or password❌");
   }
@@ -27,6 +37,7 @@ function enter() {
     console.log("You have logged into your account!");
     formElem.parentElement.classList.add("is-hidden");
     sessionStorage.setItem("logged", JSON.stringify(true));
+    renderAllCards();
   } else {
     console.log("Uncorrect login or password!");
   }
@@ -34,18 +45,23 @@ function enter() {
 
 function renderAllCards() {
   if (localStorage.getItem("todo")) {
-    todoListEl.textContent = "";
-    JSON.parse(localStorage.getItem("todo")).forEach((cardObj) => {
-      const markup = `<li class="todo__item"><div class="todo-color" style="background-color: ${cardObj.cardBorderColor}"></div><h3>${cardObj.cardTitle}</h3><p>${cardObj.cardDescription}</p></li>`;
-      todoListEl.insertAdjacentHTML("afterbegin", markup);
-    });
+    if (localStorage.getItem("todo").length >= 1) {
+      todoListEl.textContent = "";
+      JSON.parse(localStorage.getItem("todo")).forEach((cardObj) => {
+        const markup = `<li class="todo__item"><div class="todo-color" style="background-color: ${cardObj.cardBorderColor}"></div><h3>${cardObj.cardTitle}</h3><p>${cardObj.cardDescription}</p><input type=checkbox data-completed-switch /></li>`;
+        todoListEl.insertAdjacentHTML("afterbegin", markup);
+      });
+    }
   }
   if (localStorage.getItem("completed")) {
-    completedListEl.textContent = "";
-    JSON.parse(localStorage.getItem("completed")).forEach((cardObj) => {
-      const markup = `<li class="completed__item"><div class="todo-color" style="background-color: ${cardObj.borderColor}"></div><h3>${cardObj.cardTitle}</h3><p>${cardObj.cardDescription}</p></li>`;
-      todoListEl.insertAdjacentHTML("afterbegin", markup);
-    });
+    if (localStorage.getItem("completed").length >= 1) {
+      completedListEl.textContent = "";
+      JSON.parse(localStorage.getItem("completed")).forEach((cardObj) => {
+        console.log(cardObj);
+        const markup = `<li class="completed__item"><div class="todo-color" style="background-color: ${cardObj.cardBorderColor}"></div><h3>${cardObj.cardTitle}</h3><p>${cardObj.cardDescription}</p></li>`;
+        completedListEl.insertAdjacentHTML("afterbegin", markup);
+      });
+    }
   }
 }
 
@@ -84,6 +100,9 @@ document.addEventListener("DOMContentLoaded", () => {
     if (localStorage.getItem("todo") || localStorage.getItem("completed")) {
       renderAllCards();
     }
+    if (localStorage.getItem("links")) {
+      renderAllLinks();
+    }
   }
   document.querySelector("[data-add]").addEventListener("click", (evt) => {
     addCardFormElem.parentElement.classList.remove("is-hidden");
@@ -105,5 +124,127 @@ addCardFormElem.addEventListener("submit", (e) => {
     );
     renderAllCards();
     addCardFormElem.parentElement.classList.add("is-hidden");
+  }
+});
+
+document.querySelector(".todo__list").addEventListener("change", (e) => {
+  if (e.target.nodeName === "INPUT") {
+    const checkbox = e.target;
+    const clickedCardIndex = JSON.parse(localStorage.getItem("todo")).findIndex(
+      ({ cardTitle, cardDescription, cardBorderColor }) => {
+        return (
+          checkbox.parentElement.innerHTML ===
+          `<div class="todo-color" style="background-color: ${cardBorderColor}"></div><h3>${cardTitle}</h3><p>${cardDescription}</p><input type="checkbox" data-completed-switch="">`
+        );
+      }
+    );
+    const currArr = JSON.parse(localStorage.getItem("todo"));
+    const deletedCard = currArr.splice(clickedCardIndex, 1)[0];
+    console.log(deletedCard);
+    localStorage.setItem("todo", JSON.stringify(currArr));
+    let newCompletedArr = [];
+    if (localStorage.getItem("completed")) {
+      newCompletedArr = JSON.parse(localStorage.getItem("completed"));
+      console.log(newCompletedArr);
+      newCompletedArr.push(deletedCard);
+    } else {
+      newCompletedArr.push(deletedCard);
+    }
+    console.log(newCompletedArr);
+    localStorage.setItem("completed", JSON.stringify(newCompletedArr));
+    renderAllCards();
+  }
+});
+
+function renderAllLinks() {
+  if (localStorage.getItem("links").length > 0) {
+    try {
+      savedListEl.textContent = "";
+      const localStorageData = JSON.parse(localStorage.getItem("links"));
+      localStorageData.forEach((link) => {
+        savedListEl.insertAdjacentHTML(
+          "afterbegin",
+          `<li><a href=${link.linkPath}>${link.linkTitle}</a></li>`
+        );
+      });
+    } catch (err) {
+      console.log(`Parsing error: ${err}`);
+    }
+  }
+  return;
+}
+
+document.querySelector("[data-add-link]").addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (
+    e.currentTarget.title.value.length > 2 &&
+    e.currentTarget.path.value.length > 4
+  ) {
+    const linkObj = {
+      linkTitle: e.currentTarget.title.value,
+      linkPath: e.currentTarget.path.value,
+    };
+
+    if (localStorage.getItem("links")) {
+      const currArr = JSON.parse(localStorage.getItem("links"));
+      currArr.push(linkObj);
+      localStorage.setItem("links", JSON.stringify(currArr));
+    } else {
+      localStorage.setItem("links", JSON.stringify([linkObj]));
+    }
+    renderAllLinks();
+  }
+});
+
+savedListEl.addEventListener("click", (e) => {
+  // e.preventDefault();
+  // console.log(e);
+  if (e.target.nodeName === "LI") {
+    try {
+      const currLocalStrg = JSON.parse(localStorage.getItem("links"));
+      const index = currLocalStrg.findIndex((link) => {
+        const clickedElem = {
+          linkTitle: e.target.firstElementChild.textContent,
+          linkPath: e.target.firstElementChild.href,
+        };
+        console.log(clickedElem);
+        console.log(link);
+        return (
+          clickedElem.linkTitle === link.linkTitle &&
+          clickedElem.linkPath === link.linkPath
+        );
+      });
+      console.log(index);
+      const askToDo = confirm("Are you sure want to  delete this card?");
+      if (askToDo) {
+        currLocalStrg.splice(index, 1);
+      } else {
+        if (confirm("Do you want to change this card?")) {
+          savedFormEl.parentElement.classList.remove("is-hidden");
+          currLocalStrg[index];
+          savedFormEl.newTitle.value = currLocalStrg[index].linkTitle;
+          savedFormEl.newPath.value = currLocalStrg[index].linkPath;
+
+          savedFormEl.addEventListener("submit", (e) => {
+            e.preventDefault();
+            if (
+              e.currentTarget.newTitle.value !== "" &&
+              e.currentTarget.newPath.value !== ""
+            ) {
+              currLocalStrg.splice(index, 1, {
+                linkTitle: e.currentTarget.newTitle.value.trim(),
+                linkPath: e.currentTarget.newPath.value.trim(),
+              });
+            }
+          });
+        } else {
+          return;
+        }
+      }
+      localStorage.setItem("links", JSON.stringify(currLocalStrg));
+      renderAllLinks();
+    } catch (err) {
+      console.log(`Parsing error: ${err}`);
+    }
   }
 });
