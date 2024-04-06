b2a5b3a15ec2bfd38fe0f7807c32bb014ed596cf
m1xture <109725502+m1xture@users.noreply.github.com>
2024-04-06 21:33:54 +0300

    ^^

--- js/hw41/hw41.js
+++ js/hw41/hw41.js
@@ -1,15 +1,14 @@
 const refs = {
   listElem: document.querySelector("[data-list]"),
   loadMoreBtnEl: document.querySelector("[data-btn]"),
+  searchInputEl: document.querySelector("[data-search]"),
 };
 const limitImgs = 100;
-function loadImages(i) {
-  function renderCards(array) {
-    array.forEach(
-      ({ webformatURL, tags, user, userImageURL, likes, comments }) =>
-        refs.listElem.insertAdjacentHTML(
-          "beforeend",
-          `<li class="gallery__item">
+function renderCards(array) {
+  array.forEach(({ webformatURL, tags, user, userImageURL, likes, comments }) =>
+    refs.listElem.insertAdjacentHTML(
+      "beforeend",
+      `<li class="gallery__item">
            <div class="user">
     <img src=${userImageURL} alt="user avatar" class="user__avatar" />
     <p class="user__text">${user}</p>
@@ -35,10 +34,10 @@ function loadImages(i) {
     </ul>
   </div>
         </li>`
-        )
-    );
-  }
-
+    )
+  );
+}
+async function loadImages(i) {
   const params = new URLSearchParams({
     editors_choice: true,
     safesearch: true,
@@ -46,26 +45,39 @@ function loadImages(i) {
     per_page: 5,
     page: i,
   });
-  fetch(`https://pixabay.com/api/?${params}`)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    })
-    .then((data) => {
-      renderCards(data.hits);
-    })
-    .catch((err) => console.log(err));
+
+  //? with then() method
+
+  // fetch(`https://pixabay.com/api/?${params}`)
+  //   .then((response) => {
+  //     if (!response.ok) {
+  //       throw new Error(response.status);
+  //     }
+  //     return response.json();
+  //   })
+  //   .then((data) => {
+  //     renderCards(data.hits);
+  //   })
+  //   .catch((err) => console.log(err));
+
+  //? async/await method
+
+  try {
+    const resp = await fetch(`https://pixabay.com/api/?${params}`);
+    const data = await resp.json();
+    renderCards(data.hits);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 let page = 1;
-
+let searchedPages = 0;
 document.addEventListener("DOMContentLoaded", () => {
   const latestPages = localStorage.getItem("page");
   if (latestPages) {
     for (let i = 1; i <= latestPages; i++) {
-        page = i;
+      page = i;
       loadImages(i);
     }
   } else {
@@ -74,10 +86,50 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 refs.loadMoreBtnEl.addEventListener("click", () => {
-  page++;
-  loadImages(page);
-  localStorage.setItem("page", page);
-  if (page * 5 === limitImgs) {
-    refs.loadMoreBtnEl.remove();
+  if (searchedPages === 0) {
+    page++;
+    loadImages(page);
+    localStorage.setItem("page", page);
+    if (page * 5 === limitImgs) {
+      refs.loadMoreBtnEl.remove();
+    }
+  } else {
+    searchedPages++;
+    loadSearchedImgs(refs.searchInputEl.value, searchedPages);
   }
 });
+
+async function loadSearchedImgs(query, i) {
+  try {
+    const params = new URLSearchParams({
+      q: query,
+      safesearch: true,
+      key: "43028857-0f1ea2a07ec80ac5b3d9eeb9b",
+      per_page: 5,
+      page: i,
+    });
+    const resp = await fetch(`https://pixabay.com/api/?${params}`);
+    const data = await resp.json();
+    if (i === 1) refs.listElem.innerHTML = "";
+    renderCards(data.hits);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+refs.searchInputEl.addEventListener(
+  "input",
+  _.debounce((evt) => {
+    console.log("bonk");
+    if (evt.target.value === "" || evt.target.value.length >= 100) {
+      searchedPages = 0;
+      page = 1;
+      refs.listElem.innerHTML = "";
+      loadImages(page);
+      return;
+    }
+    searchedPages = 1;
+
+    loadSearchedImgs(evt.target.value, searchedPages);
+  }, 500)
+);
